fe3b86f53c4878e415792251c559c91ae9ac2031
mehmoodulhaq <mehmoodulhaq.dev@gmail.com>
2024-12-31 16:54:44 +0300

    port setting

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   // Configure CORS
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:5173',
-      'http://127.0.0.1:3000',
-      'http://127.0.0.1:5173'
-    ],
-    methods: ['GET', 'POST'],
-    credentials: true,
-  });
-
-  await app.listen(3000);
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'http://localhost:5173',
+  //     'http://127.0.0.1:3000',
+  //     'http://127.0.0.1:5173'
+  //   ],
+  //   methods: ['GET', 'POST'],
+  //   credentials: true,
+  // });
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
